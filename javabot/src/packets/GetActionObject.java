0d3fb8837a18dd7e1ad675305d2dd396be17ab21
unknown <arikwex@mit.edu>
2013-01-11 11:19:36 -0500

    Packet readers pretty good

---
+++ javabot/src/packets/GetActionObject.java
@@ -0,0 +1,40 @@
+package packets;
+
+class GetActionObject{
+
+	// GETACTION potSize numBoardCards [boardCards] numLastActions [lastActions] numLegalActions [legalActions] timebank
+
+		public int potSize;
+		public int[] boardCards;
+		public String[] lastActions;
+		public String[] legalActions;
+		public float timebank;
+
+		
+		public GetActionObject(String input){
+			String[] values = input.split(" ");
+			
+			potSize = Integer.parseInt(values[1]);
+
+			int i = 2;
+			boardCards = new int[i];
+			for (int j=0; j<boardCards.length; j++){
+				boardCards[j] = Integer.parseInt(values[i+j]);
+			}
+
+			//TODO: parse first/second player actions?
+			i += boardCards.length + 1;
+			lastActions = new String[i];
+			for (int j=0; j<lastActions.length; j++){
+				lastActions[j] = values[i+j];
+			}
+
+			i += lastActions.length + 1;
+			legalActions = new String[i];
+			for (int j=0; j<legalActions.length; j++){
+				legalActions[j] = values[i+j];
+			}
+
+			timebank = Float.parseFloat(values[i + legalActions.length + 1]);
+		}
+	}
---
+++ javabot/src/packets/NewHandObject.java
@@ -0,0 +1,27 @@
+package packets;
+
+import pokerbots.utils.HandEvaluator;
+
+class NewHandObject {
+	//NEWHAND handId button holeCard1 holeCard2 holeCard3 yourBank oppBank timeBank
+	//NEWHAND 10 true Ah Ac Ad 0 0 20.000000
+	
+	public int handId = 0;
+	public boolean button = false;
+	public int[] cards = new int[3];
+	public int myBank = 0;
+	public int oppBank = 0;
+	public double timeBank = 0;
+	
+	public NewHandObject( String input ) {
+		String[] tokens = input.split(" ");
+		handId = Integer.parseInt(tokens[1]);
+		button = Boolean.parseBoolean(tokens[2]);
+		cards[0] = HandEvaluator.stringToCard(tokens[3]);
+		cards[1] = HandEvaluator.stringToCard(tokens[4]);
+		cards[2] = HandEvaluator.stringToCard(tokens[5]);
+		myBank = Integer.parseInt(tokens[6]);
+		oppBank = Integer.parseInt(tokens[7]);
+		timeBank = Double.parseDouble(tokens[8]);
+	}
+}
