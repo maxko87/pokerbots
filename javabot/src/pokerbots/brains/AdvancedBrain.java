23473ce6ba31ec0c3c52ef1245b99937ba426d87
Max Kolysh <maxko87@gmail.com>
2013-01-30 00:01:00 -0500

    advanced bot kinda works

--- javabot/src/pokerbots/brains/AdvancedBrain.java
+++ javabot/src/pokerbots/brains/AdvancedBrain.java
@@ -13,16 +13,16 @@ public class AdvancedBrain extends GenericBrain{
 	
 	public boolean playAnywaysFlag = true;
 	
-	float val1 = 0.5f;
-	float val2 = 0.8f;
+	float val1 = 0.6f;
+	float val2 = 0.7f;
 	
-	float val3 = 0.6f;
+	float val3 = 0.8f;
 	float val4 = 0.8f;
 	
-	float val5 = 0.5f;
+	float val5 = 0.7f;
 	float val6 = 0.8f;
 	
-	float val7 = 0.5f;
+	float val7 = 0.7f;
 	float val8 = 0.8f;
 	
 
@@ -30,9 +30,10 @@ public class AdvancedBrain extends GenericBrain{
 	
 	//minimum default percentage range of winning to play/raise each street.
 	private final float[][] MIN_WIN_TO_PLAY = new float[][] {{val1, val2}, {val3, val4}, {val5, val6}, {val7, val8}};
-	private final float[] MIN_WIN_TO_RAISE = new float[] {.8f, .8f, .7f, .7f};
+	private final float[] MIN_WIN_TO_RAISE = new float[] {.8f, .8f, .75f, .75f};
 	
 
+	String state;
 	
 	public AdvancedBrain(GameObject game, MatchHistory history){
 		myGame = game;
@@ -46,16 +47,21 @@ public class AdvancedBrain extends GenericBrain{
 		
 		super.setVars(o, g, w, s);
 		
+		//reset state
+		if (street == 0)
+			this.state = "none";
+		
 		//PREFLOP ADJUSTMENTS
 		if ( street == 0 ) {
 			float raise_size = winChance * Utils.scale(opponent.getLooseness(0), .2f, .8f, 0f, 1f) * (myGame.stackSize / 10) + 2;
 			if (winChance > getMinWinChanceForRaisePreflop() && opponent.totalHandCount > 20){
+				state = "bet_state";
 				return validateAndReturn("raise",(int)(raise_size));
 			}
 			return validateAndReturn("call",0); //don't continuously reraise preflop
 		}
 		
-		if (weBeatThem() || weBluffThem() || (playAnywaysFlag && playAnyways())){
+		if ((weBeatThem() && state.equals("bet_state")) || weBluffThem() || (playAnywaysFlag && playAnyways())){
 			return betRaiseCall(false);
 		}
 		else{
@@ -69,6 +75,8 @@ public class AdvancedBrain extends GenericBrain{
 		int streetOfTheirLastAction = street;
 		float defaultOppChance = Utils.inverseScale(opponent.getLooseness(street), 0.0f, 1.0f, MIN_WIN_TO_PLAY[street][0], MIN_WIN_TO_PLAY[street][1]);
 		float oppWinChanceEstimate = defaultOppChance;
+		if (street == 1 && winChance > defaultOppChance )
+			state = "bet_state";
 		
 		if (opponent.totalHandCount < 100){
 			return (winChance > defaultOppChance);
@@ -158,11 +166,15 @@ public class AdvancedBrain extends GenericBrain{
 		
 			if ( legalAction.actionType.equalsIgnoreCase("bet") ) {
 				int bet = (int)(makeBet(legalAction.maxBet, getActionObject.potSize));
+				if (state.equals("bet_state"))
+					bet *= (street * 1.2);
 				return validateAndReturn("bet", bet);
 			}
 			else if ( legalAction.actionType.equalsIgnoreCase("raise") ) {
 				if (winChance > MIN_WIN_TO_RAISE[street]){
 					int raise = (int)(makeRaise(legalAction.maxBet, getActionObject.potSize));
+					if (state.equals("bet_state"))
+						raise *= (street * 1.2);
 					return validateAndReturn("raise", raise);
 				}
 				else{
